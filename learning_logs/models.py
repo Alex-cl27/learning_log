ef35130dadea6f96500d14b560d3ec23c548279d
cl27 <cl27v684i@gmail.com>
2023-08-21 11:53:23 +0700

    Add model 'Entry'
    add in model.py, register in admin.py
    
     Changes to be committed:
    	modified:   db.sqlite3
    	modified:   learning_logs/admin.py
    	new file:   learning_logs/migrations/0002_entry.py
    	modified:   learning_logs/models.py

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -10,3 +10,21 @@ class Topic(models.Model):
     def __str__(self):
         """ Возвращает строковое представление модели """
         return self.text
+
+
+class Entry(models.Model):
+    """ Информация по теме """
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """ Класс Meta хранит дополнительную информацию по управлению моделью """
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """ Возвращает строковое представление модели """
+        if len(self.text) <= 50:
+            return self.text
+        else:
+            return f"{self.text[:50]}..."
