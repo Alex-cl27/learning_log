f1ff58f555c99c94286dcc983751b5cf0f47f12d
cl27 <cl27v684i@gmail.com>
2023-08-22 14:49:27 +0700

    Add 'edit_entry'

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,7 +7,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.get_index, name='index'),
-    # Страницв со списком всех тем.
+    # Страница со списком всех тем.
     path('topics/', views.get_topics, name='topics'),
     # Страница с подробной информацией по отдельной теме
     path('topics/<int:topic_id>/', views.get_topic, name='topic'),
@@ -15,4 +15,6 @@ urlpatterns = [
     path('topics/add_topic/', views.add_topic, name='add_topic'),
     # Страница для добавления новой записи
     path('topics/<int:topic_id>/add_entry/', views.add_entry, name='add_entry'),
+    # Страница для редактирования записи
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry')
 ]
