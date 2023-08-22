4202fdea2e3ac053eef92f554a2e1fe1396cc8ec
cl27 <cl27v684i@gmail.com>
2023-08-22 09:32:57 +0700

    Add 'Topics'

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
-
+    # Страницв со списком всех тем.
+    path('topics/', views.topics, name='topics'),
 ]
