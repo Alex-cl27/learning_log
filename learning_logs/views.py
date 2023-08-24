5524850cd9bd8f37753a964a82fcc7c74ff997e0
cl27 <cl27v684i@gmail.com>
2023-08-24 12:39:26 +0700

    Associating a user with a created topic
    blocking browsing for non-authenticated users
    block viewing other people's topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -9,21 +11,28 @@ def get_index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def get_topics(request):
     """ Выводит список тем """
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def get_topic(request, topic_id):
     """ Выводит одну тему и все ее записи. """
     topic = Topic.objects.get(id=topic_id)
+    # Проверка владельца темы (owner)
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def add_topic(request):
     """ Добавляет новую тему """
     if request.method != 'POST':
@@ -33,7 +42,9 @@ def add_topic(request):
         # Отправлены данные POST; обработать данные.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     # Вывести пустую или недействительную форму.
@@ -41,6 +52,7 @@ def add_topic(request):
     return render(request, 'learning_logs/add_topic.html', context)
 
 
+@login_required
 def add_entry(request, topic_id):
     """ Добавляет новую запись по конкретной теме. """
     topic = Topic.objects.get(id=topic_id)
@@ -61,10 +73,14 @@ def add_entry(request, topic_id):
     return render(request, 'learning_logs/add_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """ Редактирует существующую запись """
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    # Проверка владельца темы (owner)
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Исходный запрос; форма заполняется данными текущей записи.
